d3d4fdf792932ec05618c470bb43147eec5d4112
svadayar <shylavadayar@gmail.com>
2025-11-16 00:56:21 +0000

    django files created

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,42 @@
+from djongo import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    class Meta:
+        db_table = 'teams'
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name='members')
+    is_superhero = models.BooleanField(default=False)
+    class Meta:
+        db_table = 'users'
+    def __str__(self):
+        return self.email
+
+class Activity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
+    type = models.CharField(max_length=100)
+    duration = models.IntegerField(help_text='Duration in minutes')
+    date = models.DateField()
+    class Meta:
+        db_table = 'activities'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    difficulty = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'workouts'
+    def __str__(self):
+        return self.name
+
+class Leaderboard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    class Meta:
+        db_table = 'leaderboard'
